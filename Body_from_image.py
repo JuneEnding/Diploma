7a28eb57c7d706bfcd90144b8ec225bdac28251e
n.manchuk <n.manchuk@okbsapr.ru>
2023-05-14 20:01:24 +0300

    Добывлены песледние версии программ: 1. Детектирование человека 2. Отображение изображений с опорными точками 3. Кластеризация опорных точек

---
+++ Body_from_image.py
@@ -0,0 +1,74 @@
+# From Python
+# It requires OpenCV installed for Python
+import sys
+import cv2
+import os
+from sys import platform
+import argparse
+import time
+import numpy as np
+
+t0 = time.time()
+try:
+    # Import Openpose (Windows/Ubuntu/OSX)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    try:
+        # Change these variables to point to the correct folder (Release/x64 etc.)
+        sys.path.append(dir_path + '/../bin/python/openpose/Release');
+        os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../x64/Release;' +  dir_path + '/../bin;'
+        import pyopenpose as op
+    except ImportError as e:
+        print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+        raise e
+
+    # Flags
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_dir", default="../examples/media/", help="Process a directory of images. Read all standard formats (jpg, png, bmp, etc.).")
+    parser.add_argument("--no_display", default=False, help="Enable to disable the visual display.")
+    args = parser.parse_known_args()
+
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = dict()
+    params["model_folder"] = "../models/"
+
+    # Add others in path?
+    for i in range(0, len(args[1])):
+        curr_item = args[1][i]
+        if i != len(args[1])-1: next_item = args[1][i+1]
+        else: next_item = "1"
+        if "--" in curr_item and "--" in next_item:
+            key = curr_item.replace('-','')
+            if key not in params:  params[key] = "1"
+        elif "--" in curr_item and "--" not in next_item:
+            key = curr_item.replace('-','')
+            if key not in params: params[key] = next_item
+
+    # Construct it from system arguments
+    # op.init_argv(args[1])
+    # oppython = op.OpenposePython()
+
+    # Starting OpenPose
+    opWrapper = op.WrapperPython()
+    opWrapper.configure(params)
+    opWrapper.start()
+
+    # Read frames on directory
+    imagePaths = op.get_images_on_directory(args[0].image_dir);
+    start = time.time()
+
+    # Process and display images
+    for imagePath in imagePaths:
+        datum = op.Datum()
+        imageToProcess = cv2.imread(imagePath)
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop(op.VectorDatum([datum]))
+
+        np.savetxt(imagePath + '.pose', datum.poseKeypoints[0,:,:2].astype(dtype=np.int16), fmt='%d')
+        print(imagePath + "\t" +  str(imageToProcess.shape[0])  + "\t" +  str(imageToProcess.shape[1])   + "\t" +  str(time.time() - start))
+        start = time.time()
+
+except Exception as e:
+    print(e)
+    sys.exit(-1)
+
+print("Затрачено время: ", time.time()-t0)
--- clusterization.py
+++
@@ -1,6 +0,0 @@
-
-
-class Clusterization:
-    def __init__(self, points, class_id):
-        self.points = points
-        self.class_id = class_id
